4e15b737bf19a808be9c96d15787029435a07d43
AndrewBestbier <andrewb@Admins-MacBook-Pro-2.local>
2015-04-06 17:38:24 +0200

    The student room is now linked up. All rooms are linked.

--- WebApp/js/frontPage.js
+++ WebApp/js/frontPage.js
@@ -1,47 +1,61 @@
-
-
-
-
-
 document.getElementById("moderatorButton").addEventListener("click", function() {
     var roomPassword = document.getElementById('moderatorPassword').value;
 
-    /*
-    if(test == ''){
+    
+    if(roomPassword == ''){
     	alert("Please input a password");
     } else {
-    	alert(test);
-    } */
+    	// Get a reference to our posts
+    	var ref = new Firebase("https://engaged.firebaseio.com/ModToRoomMap/"+roomPassword+"/");
 
-    
+    	// Attach an asynchronous callback to read the data at our posts reference
+    	ref.on("value", function(snapshot) {
+    	  result = snapshot.val();
 
-    // Get a reference to our posts
-    var ref = new Firebase("https://engaged.firebaseio.com/ModToRoomMap/"+roomPassword+"/");
-
-    // Attach an asynchronous callback to read the data at our posts reference
-    ref.on("value", function(snapshot) {
-      result = snapshot.val();
-
-      if(result==null){
-      	alert("This room does not exist");
-      } else {
-      	document.cookie="roomCode=" + snapshot.val().roomCode;
-      	window.location.href = 'moderator.html';
-      }
+    	  if(result==null){
+    	  	alert("This room does not exist");
+    	  } else {
+    	  	document.cookie="roomCode=" + snapshot.val().roomCode;
+    	  	window.location.href = 'moderator.html';
+    	  }
 
 
-    }, function (errorObject) {
-      console.log("The read failed: " + errorObject.code);
-    }); 
-
-
-	
-
-	
-
-	
-
-    
+    	}, function (errorObject) {
+    	  console.log("The read failed: " + errorObject.code);
+    	}); 
+    } 
 });
 
+
+document.getElementById("studentButton").addEventListener("click", function() {
+
+
+    var roomCode = document.getElementById('roomCode').value;
+
+    
+
+    
+    if(roomCode == ''){
+    	alert("Please input a room code");
+    } else {
+    	// Get a reference to our posts
+    	var ref = new Firebase("https://engaged.firebaseio.com/"+roomCode+"/");
+
+    	// Attach an asynchronous callback to read the data at our posts reference
+    	ref.on("value", function(snapshot) {
+    	  result = snapshot.val();
+
+    	  if(result==null){
+    	  	alert("This room does not exist");
+    	  } else {
+    	  	document.cookie="roomCode=" +roomCode
+    	  	window.location.href = 'student.html';
+    	  }
+
+
+    	}, function (errorObject) {
+    	  console.log("The read failed: " + errorObject.code);
+    	}); 
+    }  
+});
 
