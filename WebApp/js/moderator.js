8996b8bf8156c506574b6ee10da12ad9f7499689
AndrewBestbier <andrewb@Admins-MacBook-Pro-2.local>
2015-04-09 20:12:30 +0200

    Closed #5, forgot students and moderator

--- WebApp/js/moderator.js
+++ WebApp/js/moderator.js
@@ -31,7 +31,8 @@ app.controller("TestController", ["$scope", "$firebaseArray","$firebaseObject", 
             $scope.showQuestionDialog = function() {
                $mdDialog.show({
                   templateUrl: 'test.html',
-                  controller: DialogController
+                  controller: DialogController,
+                  onComplete: afterShowAnimation
                 })
                .then(function(question) {
                      $scope.alert = list.$add({text: question, count : 0, time: milliSeconds  });
@@ -39,6 +40,10 @@ app.controller("TestController", ["$scope", "$firebaseArray","$firebaseObject", 
 
                    }, function() {
                    });
+
+               function afterShowAnimation(scope, element, options) {
+                         document.getElementById("question-input").focus();
+                      }
             }
 
 
